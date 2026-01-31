a3f862432db08d755d16b8e41e39226b7c043126
Your Name <you@example.com>
2026-01-31 18:36:59 +0000

    ok

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-chmod -R 600 ../ansible_m1/
+chmod -R 600 ../ansible_m1/ #ansible cfg security pratice
 chmod 700 setup_ssh.sh
 pip3 install -r .devcontainer/requirements.txt --break-system-packages
-ansible-galaxy collection install community.general
+ansible-galaxy install -r .devcontainer/requirements.yml
 activate-global-python-argcomplete
