a7d470f215020546e57321452ace3cdd25c5d990
Your Name <you@example.com>
2026-02-04 17:56:23 +0000

    ok

--- publish_collection.sh
+++ publish_collection.sh
@@ -1,3 +1,5 @@
 # ansible-galaxy collection init plouik.sdv
 ansible-galaxy collection build plouik/sdv
 ansible-galaxy collection publish plouik-sdv-1.0.0.tar.gz --token 78cb6fc224a38d7ff72235e9dc96a9138e69614d
+rm -f plouik-sdv-1.0.0.tar.gz
+ansible-galaxy collection install plouik/sdv --force
